c4a3774b26649a62f9666d37b91a37c72333fac2
saeeun9325 <saeeun9325@naver.com>
2024-10-16 16:17:31 +0900

    Update 1주차 4.배열실습.cpp

--- C언어 과제/1주차 4.배열실습.cpp
+++ C언어 과제/1주차 4.배열실습.cpp
@@ -13,7 +13,7 @@ int main()
 		number[i] = (rand() % 10000) + 1;
 	}
 	
-	for (int i = 0; i < SIZE - 1; i++) {         //Á¤·ÄÇÏ´Â ºÎºĞ!!
+	for (int i = 0; i < SIZE - 1; i++) {         //ÃÂ¤Â·Ã„Ã‡ÃÂ´Ã‚ ÂºÃÂºÃ!!
 		most = i;
 		for (int j = i + 1; j < SIZE; j++) {
 			if (number[most] < number[j]) most = j;
@@ -25,7 +25,8 @@ int main()
 
 	for (int i = 0; i < SIZE; i++) {
 		printf("%5d\t", number[i]);
-		if ((i + 1) % 10 == 0) printf("\n");
+		if ((i + 1) % 10 == 0) 
+		   printf("\n");
 	}
 
 	return 0;
