6e8b10f37ea999dd461be5900640006f8bd58ca5
saeeun9325 <saeeun9325@naver.com>
2024-10-16 15:54:46 +0900

    Update 6주차 2. 구조체2.cpp

--- C언어 과제/6주차 2. 구조체2.cpp
+++ C언어 과제/6주차 2. 구조체2.cpp
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 struct point {
-	int x;
-	int y;
+    int x;
+    int y;
 };
 
-int main()
-{
-	struct point p1 = { 10, 20 };
-	struct point p2;
-	printf("구조체 p1의 멤버변수 x,y의 값 : (%d, %d)\n", p1.x, p1.y);
-	printf("구조체 p2의 멤버변수 x,y의 값 : (%d, %d)\n", p2.x, p2.y);
-	p2.x = 30;
-	p2.y = 45;
-	printf("구조체 p1의 멤버변수 x,y의 값 : (%d, %d)\n", p1.x, p1.y);
-	printf("구조체 p2의 멤버변수 x,y의 값 : (%d, %d)\n", p2.x, p2.y);
+double dist(struct point p1, struct point p2) {
+    return sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
+}
 
-	return 0;
+int main() {
+    struct point p1 = {10, 20};
+    struct point p2;
+    double d;
+    
+    p1.x = 20;
+    p2.x = 30;
+    p2.y = 40;
+    
+    printf("p1.x = %d, p1.y = %d\n", p1.x, p1.y);
+    printf("p2.x = %d, p2.y = %d\n", p2.x, p2.y);
+    
+    printf("distance of p1, p2 = %lf", dist(p1, p2));
+
+    return 0;
 }
